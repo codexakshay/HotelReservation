9b882489fb70d5d178444e6adaaa511cc196db50
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-11 11:51:19 +0530

    HotelReservation :Added Cheapest Hotel with Highest reward to customers Rating

---
+++ HotelReservationTest.java
@@ -0,0 +1,41 @@
+package com.blz.hotelreservation;
+
+import java.text.ParseException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.blz.hotelreservation.*;
+
+public class HotelReservationTest {
+
+	private static HotelReservation obj;
+
+	@BeforeClass
+	public static void createHotelReservationSystemObj() {
+		obj = new HotelReservation();
+	}
+
+	@Test
+	public void givenHotelsFindTheCheapestBestRatedForRegularCustomer_ReturnsBridgeWood() throws ParseException {
+		String result = obj.cheapestBestRated("11Sep2020", "12Sep2020", "REGULAR");
+		Assert.assertEquals("BridgeWood, Rating: 4 and Total Rates: $200", result);
+	}
+
+	@Test
+	public void givenHotelsFindTheCheapestBestRatedForRewardCustomer_ReturnsRidgeWood() throws ParseException {
+		String result = obj.cheapestBestRated("11Sep2020", "12Sep2020", "REWARD");
+		Assert.assertEquals("RidgeWood, Rating: 5 and Total Rates: $140", result);
+	}
+
+	@Test
+	public void givenHotelsFindTheBestRatedHotelForRegularCustomer_ReturnsRidgeWood() throws ParseException {
+		String result = obj.findBestRatedHotel("11Sep2020", "12Sep2020", "REGULAR");
+		Assert.assertEquals("RidgeWood & Total Rates: $370", result);
+	}
+
+	@Test
+	public void givenHotelsFindTheBestRatedHotelForRewardCustomer_ReturnsRidgeWood() throws ParseException {
+		String result = obj.findBestRatedHotel("11Sep2020", "12Sep2020", "REWARD");
+		Assert.assertEquals("RidgeWood & Total Rates: $140", result);
+	}
+}
